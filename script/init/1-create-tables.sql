1e7119becb40c95c14e29bdb749442778d6f39e4
lisache121 <lisache121@gmail.com>
2024-02-03 20:17:13 +0300

    update sql scripts

--- script/init/1-create-tables.sql
+++ script/init/1-create-tables.sql
@@ -17,7 +17,25 @@ CREATE TYPE location_enum AS ENUM (
 CREATE TYPE request_status_enum  AS ENUM (
  'DECLINED',
  'READY',
- 'IN_PROGRESS',
+ 'IN_PROGRESS'
+);
+
+CREATE TABLE IF NOT EXISTS fabric_texture (
+                                              fabric_texture_id serial PRIMARY KEY,
+                                              fabric_texture_name VARCHAR(100) NOT NULL,
+                                              CONSTRAINT unique_fabric_texture_name UNIQUE (fabric_texture_name)
+);
+CREATE TABLE IF NOT EXISTS user_spacesuit_data (
+   user_spacesuit_data_id serial PRIMARY KEY,
+   head INT NOT NULL CHECK (head > 0),
+   status request_status_enum DEFAULT  'IN_PROGRESS',
+   chest INT NOT NULL CHECK (chest > 0),
+   waist INT NOT NULL CHECK (waist > 0),
+   hips INT NOT NULL CHECK (hips > 0),
+   foot_size INT NOT NULL CHECK (foot_size > 0),
+   height INT NOT NULL CHECK (height > 0 AND height <= 300),
+   fabric_texture_id INT NOT NULL,
+   CONSTRAINT fk_user_spacesuit_data_fabric_texture FOREIGN KEY (fabric_texture_id) REFERENCES fabric_texture(fabric_texture_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS user_data (
@@ -31,8 +49,9 @@ CREATE TABLE IF NOT EXISTS user_data (
     );
 CREATE TABLE IF NOT EXISTS roles (
     role_id SERIAL PRIMARY KEY,
-    role_name text,
-)
+    role_name text
+);
+
 CREATE TABLE users (
    id SERIAL PRIMARY KEY,
    username VARCHAR(255) NOT NULL,
@@ -41,35 +60,20 @@ CREATE TABLE users (
     name VARCHAR(100) ,
     CONSTRAINT fk_user_user_spacesuit_data FOREIGN KEY (user_spacesuit_data_id) REFERENCES user_spacesuit_data(user_spacesuit_data_id) ON DELETE CASCADE,
    user_data_id INT,
-   CONSTRAINT fk_user_user_data FOREIGN KEY (user_data_id) REFERENCES user_data(user_data_id) ON DELETE CASCADE
+   CONSTRAINT fk_user_user_data FOREIGN KEY (user_data_id) REFERENCES user_data(user_data_id) ON DELETE CASCADE,
     role_id INT,
    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE
 
 );
 
-CREATE TABLE IF NOT EXISTS fabric_texture (
-    fabric_texture_id serial PRIMARY KEY,
-    fabric_texture_name VARCHAR(100) NOT NULL,
-    CONSTRAINT unique_fabric_texture_name UNIQUE (fabric_texture_name)
-    );
 
 
-CREATE TABLE IF NOT EXISTS user_spacesuit_data (
-    user_spacesuit_data_id serial PRIMARY KEY,
-    head INT NOT NULL CHECK (head > 0),
-    status request_status_enum DEFAULT "IN_PROGRESS",
-    chest INT NOT NULL CHECK (chest > 0),
-    waist INT NOT NULL CHECK (waist > 0),
-    hips INT NOT NULL CHECK (hips > 0),
-    foot_size INT NOT NULL CHECK (foot_size > 0),
-    height INT NOT NULL CHECK (height > 0 AND height <= 300),
-    fabric_texture_id INT NOT NULL,
-    CONSTRAINT fk_user_spacesuit_data_fabric_texture FOREIGN KEY (fabric_texture_id) REFERENCES fabric_texture(fabric_texture_id) ON DELETE CASCADE
-    );
+
+
 
 CREATE TABLE IF NOT EXISTS user_request (
     user_request_id serial PRIMARY KEY,
     user_spacesuit_data_id INT NOT NULL,
-    status request_status_enum DEFAULT "IN_PROGRESS",
+    status request_status_enum DEFAULT  'IN_PROGRESS',
     CONSTRAINT fk_user_request_user_spacesuit_data FOREIGN KEY (user_spacesuit_data_id) REFERENCES user_spacesuit_data(user_spacesuit_data_id) ON DELETE CASCADE
 );
